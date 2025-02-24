dc8fba0bd10d72fcecf53481678282dd6efa9fd5
Rose Mullan <rfmullan@gmail.com>
2025-02-24 14:25:11 +0000

    add endpoints test in file

---
+++ __tests__/app.test.js
@@ -0,0 +1,15 @@
+const endpointsJson = require("../endpoints.json");
+/* Set up your test imports here */
+
+/* Set up your beforeEach & afterAll functions here */
+
+describe("GET /api", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body: { endpoints } }) => {
+        expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
