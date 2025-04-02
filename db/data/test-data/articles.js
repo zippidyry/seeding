a57bed3e1c674e7d72a20c066dc55733caa71292
Rose Mullan <rfmullan@gmail.com>
2025-04-02 10:41:31 +0100

    add vote values into test articles data

--- db/data/test-data/articles.js
+++ db/data/test-data/articles.js
@@ -15,6 +15,7 @@ module.exports = [
     author: "icellusedkars",
     body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
     created_at: 1602828180000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -24,6 +25,7 @@ module.exports = [
     author: "icellusedkars",
     body: "some gifs",
     created_at: 1604394720000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -33,6 +35,7 @@ module.exports = [
     author: "rogersop",
     body: "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
     created_at: 1588731240000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -42,6 +45,7 @@ module.exports = [
     author: "rogersop",
     body: "Bastet walks amongst us, and the cats are taking arms!",
     created_at: 1596464040000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -51,6 +55,7 @@ module.exports = [
     author: "icellusedkars",
     body: "Delicious tin of cat food",
     created_at: 1602986400000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -60,6 +65,7 @@ module.exports = [
     author: "icellusedkars",
     body: "I was hungry.",
     created_at: 1578406080000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -69,6 +75,7 @@ module.exports = [
     author: "icellusedkars",
     body: "Archaeologists have uncovered a gigantic statue from the dawn of humanity, and it has an uncanny resemblance to Mitch. Surely I am not the only person who can see this?!",
     created_at: 1587089280000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -78,6 +85,7 @@ module.exports = [
     author: "butter_bridge",
     body: "Well? Think about it.",
     created_at: 1591438200000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -87,6 +95,7 @@ module.exports = [
     author: "rogersop",
     body: "Who are we kidding, there is only one, and it's Mitch!",
     created_at: 1589433300000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -96,6 +105,7 @@ module.exports = [
     author: "icellusedkars",
     body: "Having run out of ideas for articles, I am staring at the wall blankly, like a cat. Does this make me a cat?",
     created_at: 1579126860000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -105,6 +115,7 @@ module.exports = [
     author: "butter_bridge",
     body: "Have you seen the size of that thing?",
     created_at: 1602419040000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
@@ -114,6 +125,7 @@ module.exports = [
     author: "butter_bridge",
     body: "There will never be enough articles about Mitch!",
     created_at: 1602419040000,
+    votes: 0,
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
