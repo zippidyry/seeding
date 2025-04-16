96fd444afa1e59006aa3c34ea0b117f118431b05
zippidry <173050483+zippidyry@users.noreply.github.com>
2025-04-16 11:04:46 +0100

    queries

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,8 +1,5 @@
 const db = require("../connection")
-const topics = require('../data/test-data/topics')
-const users = require('../data/test-data/users')
-const articles = require('../data/test-data/articles')
-const comments = require('../data/test-data/comments')
+
 const {convertTimestampToDate,createRef} = require('./utils')
 const format = require('pg-format')
 const seed = ({ topicData, userData, articleData, commentData }) => {
@@ -21,15 +18,15 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
   }).then(()=>{
     return db.query('CREATE TABLE comments(comment_id SERIAL PRIMARY KEY, article_id INT REFERENCES articles(article_id),body TEXT,votes INT DEFAULT 0,author VARCHAR REFERENCES users(username),created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);')
   }).then(()=>{
-    const formattedTopics = topics.map((topic)=>{return [topic.slug,topic.description,topic.img_url]})
+    const formattedTopics = topicData.map((topic)=>{return [topic.slug,topic.description,topic.img_url]})
     const insertTopicsQuery = format('INSERT INTO topics(slug,description,img_url) VALUES %L',formattedTopics)
     return db.query(insertTopicsQuery)
   }).then(()=>{
-    const formattedUsers = users.map((user)=>{return [user.username,user.name,user.avatar_url]})
+    const formattedUsers = userData.map((user)=>{return [user.username,user.name,user.avatar_url]})
     const insertUsersQuery = format('INSERT INTO users(username,name,avatar_url) VALUES %L',formattedUsers)
     return db.query(insertUsersQuery)
   }).then(()=>{
-    const formattedArticles = articles.map((article)=>{
+    const formattedArticles = articleData.map((article)=>{
       const articleCorrected = convertTimestampToDate(article)
       return [articleCorrected.title,articleCorrected.topic,articleCorrected.author,articleCorrected.body,articleCorrected.created_at,articleCorrected.votes,articleCorrected.article_img_url]})
     const insertArticlesQuery = format('INSERT INTO articles(title,topic,author,body,created_at,votes,article_img_url) VALUES %L RETURNING *',formattedArticles)
@@ -38,7 +35,7 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
      // Result{rows:[{}]}
      console.log(result.rows)
     const articleRefObject = createRef(result.rows)
-    const formattedComments = comments.map((comment)=>{
+    const formattedComments = commentData.map((comment)=>{
       const article_id = articleRefObject[comment.article_title]
       const commentCorrected = convertTimestampToDate(comment)
       return [commentCorrected.body,commentCorrected.votes,commentCorrected.author,commentCorrected.created_at,article_id]})
